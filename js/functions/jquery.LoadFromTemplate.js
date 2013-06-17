43f83699b9accf0deceddb4e7d3b5b784b001fa8
Jorge del Casar <jorge.casar@gmail.com>
2013-06-17 09:57:10 +0200

    Protecting the $ Alias and Adding Scope

--- js/functions/jquery.LoadFromTemplate.js
+++ js/functions/jquery.LoadFromTemplate.js
@@ -1,7 +1,3 @@
-
-// Objeto de templates
-var handlebars_templates = {};
-
 /**
  * [LoadFromTemplate jQuery plugin that loads content from Handlebars template]
  * @param {Object}   params   [All options of the plugin]
@@ -11,51 +7,52 @@ var handlebars_templates = {};
  * params.data = JSON data location (array)
  * params.array_name = Name for rename my JSON data (used for handlebars template see the example)
  */
+(function($){
+	// Objeto de templates
+	var handlebars_templates = {};
 
-$.fn.LoadFromTemplate = function(params){
+	$.fn.LoadFromTemplate = function(params){
 
-	// Default options
-	var options = {
-		path : "templates/",
-		extension : ".html",
-		name : "template",
-		data : "data.json",
-		array_name : "client"
-	};
+		// Default options
+		var options = {
+			path : "templates/",
+			extension : ".html",
+			name : "template",
+			data : "data.json",
+			array_name : "client"
+		};
 
-	// Save this jquery object
-	var _this = $(this);
+		// Save this jquery object
+		var _this = $(this);
 
-	// Extend default options with custom options
-	$.extend(options,params);
+		// Extend default options with custom options
+		$.extend(options,params);
 
-	// Private methods of the plugin
-	var methods = {
-		// Get json data
-		getData : function(){
-			$.getJSON(options.data, function(data) {
-				var obj = {};
-				obj[options.array_name] = data;
-				_this.append(handlebars_templates[options.name](obj));
-			});
-		},
-		// Compile Handlebars Template
-		compileTemplate : function(){
-			$.get(options.path+options.name+options.extension,function(results){
-					handlebars_templates[options.name] = Handlebars.compile(results);
-					methods.getData();
-			});
+		// Private methods of the plugin
+		var methods = {
+			// Get json data
+			getData : function(){
+				$.getJSON(options.data, function(data) {
+					var obj = {};
+					obj[options.array_name] = data;
+					_this.append(handlebars_templates[options.name](obj));
+				});
+			},
+			// Compile Handlebars Template
+			compileTemplate : function(){
+				$.get(options.path+options.name+options.extension,function(results){
+						handlebars_templates[options.name] = Handlebars.compile(results);
+						methods.getData();
+				});
+			}
+		};
+
+		if (typeof handlebars_templates[options.name] == "function"){
+			// If the template is preloaded data compiled
+			methods.getData();
+		}else{
+			// If the themplate isn't compiled I compile
+			methods.compileTemplate();
 		}
 	};
-
-	if (typeof handlebars_templates[options.name] == "function"){
-		// If the template is preloaded data compiled
-		methods.getData();
-	}else{
-		// If the themplate isn't compiled I compile
-		methods.compileTemplate();
-	}
-
-
-
-};
+}(jQuery));
